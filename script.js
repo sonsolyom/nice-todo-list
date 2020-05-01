eb28e6b1b2adfeb23116ba5ca1f1a0ae81797a62
sonsolyom <nagydsolyom@gmail.com>
2020-05-01 20:44:26 +0200

    have the 1/3 func, but have to find correct selector

--- script.js
+++ script.js
@@ -1,24 +1,79 @@
-
-    $('.todo-btn').click(() => { 
+$('.todo-btn').click(() => {
+        
+    if($('.todo-input').val() !== '') {        
         $('.todo-list').append(
-                `        
-                <li>${$('.todo-input').val()}
+            '<i class="far fa-check-circle"></i>'   
+        );
+        $('.todo-list').append(
+            '<i class="fas fa-check-circle complete hidden"></i>'   
+        );
+        $('.todo-list').append(
+            '<span class="fas fa-trash"></span>'
+        );
+        
+        $('.todo-list').append(
+            `<li>${$('.todo-input').val()}</li>`
+        );
+        $('li:last-child').click((event) => {
+            $(event.target).toggleClass('completed-text');          
+                $('li:last-child>i:last-child').toggleClass('hidden');
+            
+        });
+        
+    } else {
+        alert('Please write something firtst! :)');
+    }
+    $('.todo-input').val('');
+});
+
+
+
+
+
+
+
+
+
+
+/*
+
+$('.li:last-child').click((event) => {
+    $(event.$('span:last-child')).toggleClass('far fa-check-circle');
+    $(event.$('span:last-child')    ).toggleClass('fas fa-check-circle complete');
+})
+
+
+
+
+
+
+
+
+
+
+
+$('.todo-list').append(
+                `      
                 <span class="far fa-check-circle"></span>
-                <span class="fas fa-trash"></span>
-                
+                <span class="fas fa-trash"></span>  
+                <li>${$('.todo-input').val()}
                 </li>
                 `
             );
-            $('li:last-child',).click((event) => {
+            $('li:last-child').click((event) => {
                 $(event.target).toggleClass('completed-text');
             
-                    $('li>span:first-child').toggleClass('far fa-check-circle');
-                    $('li>span:first-child').toggleClass('fas fa-check-circle complete');
+                    $('span:first-child').toggleClass('far fa-check-circle');
+                    $('span:first-child').toggleClass('fas fa-check-circle complete');
             
                 });
-        
-        $('.todo-input').val('');
-    });
+
+
+
+
+
+
+*/
 
 
 
